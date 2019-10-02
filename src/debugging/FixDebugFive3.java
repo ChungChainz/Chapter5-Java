0a072589cbac0a672433650ff685989052c96edb
Chris Molis <chrismolis12@gmail.com>
2019-10-02 08:39:55 -0500

    fix debugging

--- src/debugging/FixDebugFive3.java
+++ src/debugging/FixDebugFive3.java
@@ -24,12 +24,11 @@ public class FixDebugFive3
       Scanner input = new Scanner(System.in);
       System.out.println("Please enter item number");
       item = input.nextInt();
-      if(item < LOW) output = "Item number too low";
-      else
-if(item > HIGH)
-output = "Item number too high";
-      else
-if(item > CUTOFF)
+      if(item < LOW)
+         output = "Item number too low";
+      else if(item > HIGH)
+         output = "Item number too high";
+      else if(item < CUTOFF)
              output = "Valid - in Automotive Department";
           else
              output = "Valid - Item in Housewares Department";
